c8a7e73c4465a98c3108d9a48601147fd5e954a6
nehabagal <nehabagal171@gmail.com>
2022-10-06 07:30:21 +0530

    JS Variables

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+var my_apple; // Variable declaration
+my_apple  = "Apple"; //variable initialization
+
+var my_name = "Neha"; //variable declaration & intialization on the same line
+console.log(my_name);
+console.log(my_apple);
